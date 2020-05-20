4ba5c636b18dec0ee8d80ac43fa1ddf970b62ff8
Jack <ldhontheway@live.com>
2020-05-20 22:39:45 +0800

    array

---
+++ ArrayList.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+	
+	printf("%f ", PI);	
+	
+	return 0;
+}
--- main.c
+++ main.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "array.h"
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-//int max(int a , int b);  如果没有原型声明 编译器会猜 
-
-#include "max.h"
-
 int main(int argc, char *argv[]) {
+	
+	
+	Array a = array_create(100);
+	
+	*array_at(&a,0) = 10;
 
-//	int a = 5,b = 6;
-	double a = 1.0, b =2.0;
-	
-	printf("%d \n" , max(a,b));
-	
+
+	array_free(&a);
 	
 	return 0;
 }
