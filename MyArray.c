b297ea72004dcf8e8cd69113e7f141e3b7b3825f
Jack <ldhontheway@live.com>
2020-05-26 14:29:36 +0800

    MyArray completed

--- MyArray.c
+++ MyArray.c
@@ -2,6 +2,16 @@
 #include <malloc.h>
 #include <stdbool.h>
 
+/*
+typedef struct Array {
+
+} *PArr,Arr;
+
+PArr ar;  // ==>  struct Array * ar;
+Arr arr;  // ==>  struct Array arr;
+
+*/
+
 struct Array {
 	int * pBase;
 	int len;
@@ -10,25 +20,35 @@ struct Array {
 
 void init_arr(struct Array * pArr,int length);
 bool append_arr(struct Array *pArr,int number);
-bool insert_arr((struct Array *pArr,int pos,int val);
-bool delete_arr();
-int get();
+bool insert_arr(struct Array *pArr,int pos,int val);
+bool delete_arr(struct Array *pArr,int pos,int *pVal);
+int get(struct Array *pArr,int pos);
 bool is_empty(struct Array *pArr);
 bool is_full(struct Array *pArr);
-void sort_arr();
-void show_arr();
-void inversion_arr();
+void sort_arr(struct Array *pArr);
+void show_arr(struct Array *pArr);
+void inversion_arr(struct Array *pArr);
 
 int main(int argslen, char * args []) {
 	
 	struct Array arr;
 	
-	init_arr(&arr,0);
+	init_arr(&arr,10);
 	
 	append_arr(&arr,1);
 	append_arr(&arr,2);
 	append_arr(&arr,3);
-	
+
+	// insert_arr(&arr,3,9);
+
+	inversion_arr(&arr);
+
+	// int deleted;
+	// delete_arr(&arr,1,&deleted);
+	// printf("deleted:   %d  \n",deleted);
+
+	sort_arr(&arr);
+
 	show_arr(&arr);
 	
 	return 0;
@@ -46,17 +66,11 @@ void init_arr(struct Array * pArr,int length) {
 }
 
 bool is_empty(struct Array *pArr) {
-	if(pArr->cnt == 0) {
-		return true;	
-	}
-	return false;
+	return pArr->cnt == 0;
 }
 
 bool is_full(struct Array *pArr) {
-	if(pArr->cnt == pArr->len) {
-		return true;
-	} 
-	return false;
+	return pArr->cnt == pArr->len;
 }
 
 void show_arr(struct Array *pArr) {
@@ -80,12 +94,12 @@ bool append_arr(struct Array *pArr,int number) {
 //	pArr->cnt += 1;
 }
 
-bool insert_arr((struct Array *pArr,int pos,int val){
+bool insert_arr(struct Array *pArr,int pos,int val){
 	if(is_full(pArr)) {
 		printf("Array is full\n");
 		return false;
 	}
-	if(pos < 1) {
+	if(pos < 1 || pos > pArr->cnt) {
 		return false;
 	}
 	int i ;
@@ -93,4 +107,55 @@ bool insert_arr((struct Array *pArr,int pos,int val){
 		pArr->pBase[i + 1] = pArr->pBase[i];
 	}
 	pArr->pBase[pos - 1] = val;
+	(pArr->cnt)++;
+}
+
+bool delete_arr(struct Array *pArr,int pos,int *pVal) {
+
+	if(is_empty(pArr)) {
+		printf("Array is empty\n");
+		return false;
+	}
+	if(pos < 1 || pos > pArr->cnt) {
+		return false;
+	}
+
+	*pVal = pArr->pBase[pos - 1];
+
+	int i ;
+	for(i = pos; i < pArr->cnt; i++) {
+		pArr->pBase[i - 1] = pArr->pBase[i];
+	}
+	
+	(pArr->cnt)--;
+	return true;
+}
+
+
+void inversion_arr(struct Array *pArr) {
+	int i = 0;
+	int j = pArr->cnt - 1;
+
+	while(i < j) {
+		int t = pArr->pBase[i];
+		pArr->pBase[i] = pArr->pBase[j];
+		pArr->pBase[j] = t;
+
+		i++;
+		j--;
+	}
+}
+
+
+void sort_arr(struct Array *pArr) {
+	int i,j;
+	for(i = 0; i < pArr->cnt; i++) {
+		for(j = i + 1; j < pArr->cnt; j++) {
+			if(pArr->pBase[i] > pArr->pBase[j]) {
+				int t = pArr->pBase[i];
+				pArr->pBase[i] = pArr->pBase[j];
+				pArr->pBase[j] = t;
+			}
+		}
+	}
 }
