44c743ada75d6c71cd66d9a73121f98ac692a36b
cherryling25 <49121474+cherryling25@users.noreply.github.com>
2020-05-28 07:03:57 +0800

    MyLinkedList completed

--- MyLinkedList.c
+++ MyLinkedList.c
@@ -13,7 +13,7 @@ void traverse_list(PNODE pNode);
 bool is_empty(PNODE pNode);
 int length_list(PNODE pNode);
 bool insert_list(PNODE,int,int);
-bool delete_list(PNODE, int,int);
+bool delete_list(PNODE pHead, int pos,int * value);
 void sort_list(PNODE);
 
 int main(int argslen,char * args []) {
@@ -31,6 +31,15 @@ int main(int argslen,char * args []) {
 	sort_list(pHead);
 	traverse_list(pHead);
 	
+	insert_list(pHead,5,99);
+	traverse_list(pHead);
+	
+	int deletedValue;
+	delete_list(pHead,1,&deletedValue);
+	
+	printf("deleted : %d \n", deletedValue);
+		traverse_list(pHead);
+	
 	return 0;
 }
 
@@ -47,7 +56,6 @@ PNODE create_list(void) {
 		exit(-1);
 	}
 	PNODE pTail = pHead;
-	printf("current pHead-pNext : %p \n",pHead->pNext);
 	pTail->pNext = NULL;//这里之所以要设为NULL,是因为 malloc 分配内存之后 pNext默认会分配内存，这里在循环的时候 会死循环   pTail的 pNext 一定是NULL 
 	
 	printf("请输入链表长度: ");
@@ -94,12 +102,53 @@ int length_list(PNODE pNode) {
 	return len;
 }
 
-bool insert_list(PNODE pHead,int i,int value) {
-	return false;
+bool insert_list(PNODE pHead,int pos,int value) {
+
+	if(pos > length_list(pHead)) {
+		printf("insert outofbound error.");
+		return false;
+	}
+	
+	PNODE pNew = (PNODE)malloc(sizeof(NODE));
+//	pNew->pNext = NULL; 这里不需要设置NULL 因为它不会是最后一个 
+	pNew->data = value;
+	
+	PNODE p = pHead;
+	int j = 0;
+	while( p != NULL) {
+		j ++;
+		if(j == pos) {
+			pNew->pNext = p->pNext;
+			p->pNext = pNew;
+		}
+		p = p->pNext;
+	}
+	
+	return true;
 }
 
-bool delete_list(PNODE pHead, int i,int value) {
-	return false;
+bool delete_list(PNODE pHead, int pos,int * value) {
+	if(pos > length_list(pHead)) {
+		printf("delete outofbound error.");
+		return false;
+	}
+	
+	PNODE p = pHead;
+	int j = 0;
+	while( p != NULL) {
+		j ++;
+		if(j == pos) {
+			PNODE q = p->pNext;
+			*value = q->data;
+			p->pNext = p->pNext->pNext;
+			free(q);
+			q = NULL;
+			break;
+		}
+		p = p->pNext;
+	}
+	
+	return true;
 }
 
 void sort_list(PNODE pHead) {
