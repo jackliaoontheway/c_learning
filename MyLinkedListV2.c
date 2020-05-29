992df4c7ae96b03cba4a6c20c570b972d89cd191
jack <ldhontheway@live.com>
2020-05-29 08:01:48 +0800

    insert
    insert

--- MyLinkedListV2.c
+++ MyLinkedListV2.c
@@ -12,7 +12,7 @@ bool append(Node **pTail,int val);
 void traverse(Node *);
 bool isEmpty(Node *);
 int length(Node *);
-bool insert(Node *,int pos,int val);
+bool insert(Node *,Node **pTail,int pos,int val);
 bool remove_(Node *, int pos,int *val);
 void sort(Node *);
 
@@ -30,12 +30,27 @@ int main(int argslen, char * args []) {
 	
 	traverse(pHead);
 	
+	int len = length(pHead);
+	printf("length : %d \n",len);
+	
+	insert(pHead,&pTail,3,99);
+	
+	traverse(pHead);
+	
+	append(&pTail,88);
+	append(&pTail,88);
+	
+	traverse(pHead);
+	
 	return 0;
 }
 
 
 Node* create(void) {
+	//Node head;
+	//Node *pHead = &head; ???
 	Node *pHead = (Node*)malloc(sizeof(Node));
+	
 	pHead->pNext = NULL;
 	return pHead;
 }
@@ -67,10 +82,54 @@ bool isEmpty(Node *pHead) {
 	return pHead->pNext == NULL;
 }
 
-int length(Node *) {
+int length(Node *pHead) {
+	int length;
+		
+	Node *p = pHead->pNext;
+	while(p != NULL) {
+		length++;
+		p = p->pNext;
+	}
 	
+	return length;
 }
 
+bool insert(Node *pHead,Node **pTail,int pos,int val) {
+	
+	if(pos < 1) {
+		printf("insert error pos < 1");
+		return false;
+	}
+	
+	Node *pNew = (Node*)malloc(sizeof(Node));
+	pNew->data = val;
+	pNew->pNext = NULL;
+	
+	int i = 0;
+		
+	Node *p = pHead;
+	while(p != NULL && i < pos - 1) {
+		i++;
+		p = p->pNext;
+	}
+	
+	pNew->pNext = p->pNext;
+	p->pNext = pNew;
+	
+	if(pNew->pNext == NULL) {
+		*pTail = pNew;
+	}
+	
+	return true;
+}
+
+bool remove_(Node *pHead, int pos,int *val) {
+	return true;
+}
+
+void sort(Node *pHead) {
+	
+}
 
 
 
