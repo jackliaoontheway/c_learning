c08dfe862ed69158cc2f411655c1f48e5ef05ba8
Jack <ldhontheway@live.com>
2020-06-15 17:42:12 +0800

    201910

--- 201910.c
+++ 201910.c
@@ -3,33 +3,61 @@
 
 void min(int a, int b);
 void gcd(int a, int b);
-int gcd2(int a,int b);
+int gcd2(int a, int b);
 void max(int a, int b);
+
+void sxh();
 
 int main(int argslen, char *args[])
 {
-    int a, b;
-    printf("type 2 numbers :");
-    scanf("%d %d", &a, &b);
-    gcd(a, b);
-    int x = gcd2(a, b);
-    printf("gcd2 : %d \n", x);
-    min(a, b);
-    max(a, b);
+    // int a, b;
+    // printf("type 2 numbers :");
+    // scanf("%d %d", &a, &b);
+    // gcd(a, b);
+    // int x = gcd2(a, b);
+    // printf("gcd2 : %d \n", x);
+    // min(a, b);
+    // max(a, b);
+
+    sxh();
+
     return 0;
 }
 
-int gcd2(int a,int b) {
-    if(b == 0) {
+void sxh()
+{
+    int i = 100;
+    while (i < 1000)
+    {
+        int temp = i;
+        int x = temp % 10;
+        temp /= 10;
+        int y = temp % 10;
+        temp /= 10;
+        int z = temp % 10;
+        
+        if (i == ((x * x * x) + (y * y * y) + (z * z * z)))
+        {
+            printf("%d  ", i);
+        }
+        i++;
+    }
+}
+
+int gcd2(int a, int b)
+{
+    if (b == 0)
+    {
         return a;
     }
-    return gcd2(b,a % b);
+    return gcd2(b, a % b);
 }
 
 void gcd(int a, int b)
 {
     int i = b;
-    while(a % b != 0) {
+    while (a % b != 0)
+    {
         i = a % b;
         a = b;
         b = i;
